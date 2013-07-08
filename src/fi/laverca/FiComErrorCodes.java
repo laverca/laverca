cad66e38a84dd272f75d505e74cfb4c7bd7c7d7a
mikaeljl <mikaeljl@8882375b-f798-4923-9e66-ffa807b0447f>
2013-07-08 11:38:51 +0000

    Bug #16 Great license update.

--- src/fi/laverca/FiComErrorCodes.java
+++ src/fi/laverca/FiComErrorCodes.java
@@ -1,3 +1,22 @@
+/* ==========================================
+ * Laverca Project
+ * https://sourceforge.net/projects/laverca/
+ * ==========================================
+ * Copyright 2013 Laverca Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fi.laverca;
 
 import javax.xml.namespace.QName;
@@ -8,8 +27,6 @@ import org.apache.commons.logging.LogFactory;
 
 /**
  * As per FiCom Soveltamisohje v2.0
- * 
- * @author Asko Saura (asko@methics.fi)
  */
 public class FiComErrorCodes {
     private static final Log log = LogFactory.getLog(FiComErrorCodes.class);
