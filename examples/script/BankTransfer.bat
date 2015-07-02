fc26bca1e2040e69d1dab10a5046cfc4eaa11374
Hemuu <eemeli.miettinen@methics.fi>
2015-07-02 21:10:32 +0300

    Removed deprecated scripts

--- examples/script/BankTransfer.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0..
-java -classpath ../libs/*;*;../* fi.laverca.examples.BankTransfer
-pause
--- examples/script/FiComSigReqCaller.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd `dirname $0`/..
-java -classpath '../libs/*:*:../*' fi.laverca.examples.FiComSigReqCaller
