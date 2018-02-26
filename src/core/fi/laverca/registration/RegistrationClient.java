858cae3aabc7d6e91efac4c43dede96178d9f41d
Miettinen Eemeli, Methics <eemeli.miettinen@methics.fi>
2018-02-26 15:30:40 +0200

    Improve registration api (added new send() method)

--- src/core/fi/laverca/registration/RegistrationClient.java
+++ src/core/fi/laverca/registration/RegistrationClient.java
@@ -1,6 +1,26 @@
+/* ==========================================
+ * Laverca Project
+ * https://sourceforge.net/projects/laverca/
+ * ==========================================
+ * Copyright 2015 Laverca Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fi.laverca.registration;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import fi.laverca.jaxb.mreg.RegistrationInput;
@@ -46,6 +66,20 @@ public class RegistrationClient {
         MSSRegistrationReq  _req  = req.toMSSReq(this.client);
         MSSRegistrationResp _resp = this.client.send(_req, req.context);
         return new MregResponse(_resp, req.context);
+    }
+    
+    /**
+     * Send a multi-operation MSS_RegistrationRequest (with MReg extension)
+     * <p>Reads the target and AP_TransID from {@code req}. Adds a new RegistrationInput for each element in {@code additionalRequests}
+     *
+     * @param req MReg Request
+     * @param additionalRequests Additional requests (only operation content is fetched from these - target is ignored
+     * @return parsed MReg response
+     * @throws IOException
+     */
+    public MregResponse send(final MregRequest req,
+                             final MregRequest ... additionalRequests) throws IOException {
+         return this.send(req, Arrays.asList(additionalRequests));
     }
     
     /**
