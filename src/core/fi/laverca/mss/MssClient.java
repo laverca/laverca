073699a3aa0ef67fcbc7fd04f25916915ff7a4f6
Miettinen Eemeli, Methics <eemeli.miettinen@methics.fi>
2019-03-13 10:30:06 +0200

    Fix MssClient thread safety issues

--- src/core/fi/laverca/mss/MssClient.java
+++ src/core/fi/laverca/mss/MssClient.java
@@ -311,7 +311,7 @@ public class MssClient {
         }
     }
     
-    private static void marshallerInit() {
+    private static synchronized void marshallerInit() {
         
         if (marshallerInitDone) return;
 
